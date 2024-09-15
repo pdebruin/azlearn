54d5952225a53868b9d0ca27396830bd3f7d8b7d
Pieter de Bruin <pdebruin@hotmail.com>
2024-09-15 09:16:20 +0200

    starting

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+Console.WriteLine("AzLearn brings Microsoft Learn to the command-line!");
+Console.WriteLine("---------------------------------------------------");
+Console.WriteLine("Use 'azlearn -h' to see available commands or go to https://aka.ms/azlearn.");
+Console.WriteLine("");
+Console.WriteLine("Here are the base commands:");
+Console.WriteLine("");
+Console.WriteLine("    docs    : Documentation");
+Console.WriteLine("    find    : Search for examples");
+Console.WriteLine("    version : Show the versions ");
+Console.WriteLine("");
+
